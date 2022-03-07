1d09aafe4a1a0ed0840aa8ed94dd984dce6dbf09
Petro Osipenko <osipenko.petro@gmail.com>
2022-03-07 14:00:40 +0200

    Init commit

---
+++ src/main/java/tech/pragmat/sldev10554githeadcommitissue/Sldev10554GitHeadCommitIssueApplication.java
@@ -0,0 +1,13 @@
+package tech.pragmat.sldev10554githeadcommitissue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sldev10554GitHeadCommitIssueApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sldev10554GitHeadCommitIssueApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tech/pragmat/sldev10554githeadcommitissue/Sldev10554GitHeadCommitIssueApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.pragmat.sldev10554githeadcommitissue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sldev10554GitHeadCommitIssueApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
